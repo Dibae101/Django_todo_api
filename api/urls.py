2aacfe6182b977296aefb8ec7fd6a108e05c3a08
Dibya Darshan Khanal <dibaekhanal101.com>
2021-07-27 18:50:14 +0545

    task-detail

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from .import views
 
 urlpatterns = [
     path('', views.Todo, name="Todo"),
+    path('task-list/', views.taskList, name="task-list"),
+    path('task-detail/<str:pk>/', views.taskDetail, name="task-detail")
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,33 @@
+from api.models import Task
 from django.http.response import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
 
-# Create your views here.
 
+@api_view(['GET'])  #django reset api decorator
 def Todo(request):
-    return JsonResponse("API POINT", safe=False)
+    api_urls ={
+        'List': '/tasl=k-list',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/', 
+        }
+    return Response(api_urls)
+
+
+@api_view(['GET'])  #django reset api decorator
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])  #django reset api decorator
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+
